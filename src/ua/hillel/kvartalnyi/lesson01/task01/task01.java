920274af93b08b95efaa7aec49babe4dde7fea40
Anton Kvartalnyi <anton.kvart@gmail.com>
2018-12-06 00:10:02 +0200

    Initial commit

---
+++ src/ua/hillel/kvartalnyi/lesson01/task01/task01.java
@@ -0,0 +1,22 @@
+package ua.hillel.kvartalnyi.lesson01.task01;
+
+public class task01 {
+    public static void main(String[] args) {
+        String a =   "      *       *   *" ;
+        String b =   "     * *      *  *" ;
+        String c =   "    *   *     * * ";
+        String d =   "   *******    **";
+        String e =   "  *       *   * *";
+        String f =   " *         *  *  *";
+        String g =   "*           * *   *";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
+
+
